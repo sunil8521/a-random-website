50ba41e6bc438498de973e6a2a66a312e41e85d6
sunil8521 <98368191+sunil8521@users.noreply.github.com>
2023-06-27 18:58:46 +0000

    new

--- main.py
+++ main.py
@@ -1,8 +1,34 @@
-from flask import Flask
-app=Flask(__name__)
-
+from flask import Flask, render_template
+app=Flask(__name__,template_folder='temp')
+joblist=[
+    {
+      'id':1,
+      'tittle':'web-devloper',
+      'salary':'12,000'
+    },
+    {
+      'id':2,
+      'tittle':'data-scientist',
+      'salary':'90,000'
+    },
+    {
+      'id':3,
+      'tittle':'software-devloper',
+      'salary':'1,20,000'
+    },
+    {
+      'id':4,
+      'tittle':'hacker',
+      'salary':'12,20,000'
+    },
+    {
+      'id':5,
+      'tittle':'CEO',
+      'salary':'5,00,000'
+    }
+]
 @app.route('/')
 def hell():
-  return 'hello, world'
+  return render_template('home.html',job=joblist,sitename='jobdekho.com')
 if __name__=="__main__":
   app.run(host='0.0.0.0',debug=True)
