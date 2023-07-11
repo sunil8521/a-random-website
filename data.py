fe75c2a789e536f802f6955d1900d137c2e29768
sunil8521 <98368191+sunil8521@users.noreply.github.com>
2023-07-11 19:42:02 +0000

    connect to db

---
+++ data.py
@@ -0,0 +1,24 @@
+import mysql.connector
+import os
+
+dataBase = mysql.connector.connect(
+    host=os.environ['host_key'],
+    user=os.environ['admin_key'],
+    passwd=os.environ['pass_key'],
+    database='newdb'
+)
+def store():
+    object = dataBase.cursor()
+    query = "select * from jobs"
+    object.execute(query)
+    myresult = object.fetchall()
+    jobs_dict = []
+    for row in myresult:
+        job_dict = {}
+        for index, value in enumerate(row):
+            column_name = object.column_names[index]
+            job_dict[column_name] = value
+        jobs_dict.append(job_dict)
+    dataBase.close()
+    return jobs_dict
+
--- main.py
+++ main.py
@@ -1,48 +1,51 @@
 from flask import Flask, render_template,jsonify
+from data import store
 app=Flask(__name__,template_folder='all_details')
-joblist=[
-    {
-      'id':1,
-      'tittle':'web-devloper',
-      'salary':'12,000'
-    },
-    {
-      'id':2,
-      'tittle':'data-scientist',
-      'salary':'90,000'
-    },
-    {
-      'id':3,
-      'tittle':'software-devloper',
-      'salary':'1,20,000'
-    },
-    {
-      'id':4,
-      'tittle':'hacker',
-      'salary':'12,20,000'
-    },
-    {
-      'id':5,
-      'tittle':'CEO',
-      'salary':'5,00,000'
-    },
-    {
-      'id':6,
-      'tittle':'Engineer',
-      'salary':'1,00,000'
-    },
-    {
-      'id':7,
-      'tittle':'Produc Management',
-      'salary':'2,00,000'
-    }
-]
+# joblist=[
+#     {
+#       'id':1,
+#       'tittle':'web-devloper',
+#       'salary':'12,000'
+#     },
+#     {
+#       'id':2,
+#       'tittle':'data-scientist',
+#       'salary':'90,000'
+#     },
+#     {
+#       'id':3,
+#       'tittle':'software-devloper',
+#       'salary':'1,20,000'
+#     },
+#     {
+#       'id':4,
+#       'tittle':'hacker',
+#       'salary':'12,20,000'
+#     },
+#     {
+#       'id':5,
+#       'tittle':'CEO',
+#       'salary':'5,00,000'
+#     },
+#     {
+#       'id':6,
+#       'tittle':'Engineer',
+#       'salary':'1,00,000'
+#     },
+#     {
+#       'id':7,
+#       'tittle':'Product Management',
+#       'salary':'2,00,000'
+#     }
+# ]
 @app.route('/')
 def hell():
+  joblist=store()
   return render_template('home.html',job=joblist,sitename='job')
 
 @app.route('/api/joblist')
 def jobs():
+  joblist=store()
   return jsonify(joblist)
 if __name__=="__main__":
   app.run(host='0.0.0.0',debug=True)
